bd6502a6cd7f210cdc1cfdf3bc3bf0f05a6f587e
Yury Lytvynenko <yury.lytvynenko@kredito.de>
2015-01-10 03:51:13 +0200

    add integration test

---
+++ web/src/test/java/com/yl/maven/web/MarkdownResourceTest.java
@@ -0,0 +1,11 @@
+package com.yl.maven.web;
+
+import org.testng.annotations.Test;
+
+public class MarkdownResourceTest {
+
+    @Test
+    public void testParse() throws Exception {
+//        given().body("## Lorem ipsum").when().post().then().body(equalTo("<h2>Lorem ipsum</h2>"));
+    }
+}
